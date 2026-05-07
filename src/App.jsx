6fd10d3a104a4a8a3eb80dcfe60067e43b580c19
federicoacker <acker.federico@gmail.com>
2026-05-07 13:43:53 +0200

    Commit iniziale con main e header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header.jsx";
+import Main from "./components/Main/Main.jsx";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    </>
+  );
+}
+export default App;
